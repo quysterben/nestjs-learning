2dd983d390f6f06a3cc966aec6a0f9a94df0f752
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2024-06-03 13:14:07 +0700

    feat: middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
 import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
+
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
+  }
+}
