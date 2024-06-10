9ad64bf9c2855c5fec2c14dec61621201ebae590
quy.hoang <quy.hoang@kiaisoft.com.vn>
2024-06-10 15:43:09 +0700

    feat: add cloudinary module

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
-import { CloudinaryController } from './cloudinary.controller';
+import { CloudinaryProvider } from './cloudinary';
 
 @Module({
-  providers: [CloudinaryService],
-  controllers: [CloudinaryController],
+  providers: [CloudinaryService, CloudinaryProvider],
+  exports: [CloudinaryService, CloudinaryProvider],
 })
 export class CloudinaryModule {}
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryResponse } from './types/cloudinary-response';
 
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  async uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+    const folderPath = process.env.FOLDER_PATH || `res/images`;
+
+    try {
+      const uploadedFile = await cloudinary.uploader.upload(file.path, {
+        folder: folderPath,
+        resource_type: 'auto',
+      });
+      if (!uploadedFile) {
+        throw new Error('File not uploaded');
+      }
+      return uploadedFile;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  async destroyFile(url: string): Promise<CloudinaryResponse> {
+    const publishedId = String(process.env.FOLDER_PATH + url.split('/').pop());
+
+    try {
+      const deletedFile = await cloudinary.uploader.destroy(publishedId);
+      if (!deletedFile) {
+        throw new Error('File not deleted');
+      }
+      return deletedFile;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+}
